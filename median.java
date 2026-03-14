ccbb280632e7cfbdb55d202831448ffc895ed960
Priyanka T <priyankathiyagu11@gmail.com>
2026-03-14 13:07:30 +0530

    Create median.java

---
+++ median.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr  = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        if(n%2!=0){
+            System.out.printf("%.2f",(double)(arr[n/2]));
+        }
+        else{
+            double median = (arr[n/2]+arr[(n/2)-1])/2.0; 
+            System.out.printf("%.2f",(double)(median));
+        }
+    }
+}
